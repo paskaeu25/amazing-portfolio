edf26df57e4417afd8dc1a8e8281fc6d80e84361
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2023-11-21 09:12:05 +0200

    initial commmit

---
+++ src/data/Data.js
@@ -0,0 +1,167 @@
+import {
+  faHome,
+  faEnvelope,
+  faUserGraduate,
+  faFolder,
+} from '@fortawesome/free-solid-svg-icons';
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Home',
+    icon: faHome,
+    path: '/',
+  },
+  {
+    id: 2,
+    name: `About`,
+    icon: faUserGraduate,
+    path: '/About',
+  },
+  {
+    id: 3,
+    name: 'Portfolio',
+    icon: faFolder,
+    path: '/portfolio',
+  },
+  {
+    id: 4,
+    name: 'Contact',
+    icon: faEnvelope,
+    path: '/Contact',
+  },
+];
+
+export const experience = [
+  {
+    id: 1,
+    no: '2+',
+    title: 'Years Experience',
+  },
+  {
+    id: 2,
+    no: '10+',
+    title: 'Complete Projects',
+  },
+  {
+    id: 3,
+    no: '3+',
+    title: 'Happy Clintes',
+  },
+];
+
+import image1 from '../../src/assets/python.png';
+import image2 from '../../src/assets/react.png';
+import image3 from '../../src/assets/node.png';
+import image4 from '../../src/assets/figma.png';
+import image5 from '../../src/assets/ps.png';
+import image6 from '../../src/assets/sketch.png';
+export const tools = [
+  {
+    id: 1,
+    img: image1,
+  },
+  {
+    id: 2,
+    img: image2,
+  },
+  {
+    id: 3,
+    img: image3,
+  },
+  {
+    id: 4,
+    img: image4,
+  },
+  {
+    id: 5,
+    img: image5,
+  },
+  {
+    id: 6,
+    img: image6,
+  },
+];
+
+import {
+  faCodeBranch,
+  faCode,
+  faCodeCompare,
+} from '@fortawesome/free-solid-svg-icons';
+export const services = [
+  {
+    id: 1,
+    icon: faCode,
+    name: 'Web Development',
+    des: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione officiis iusto nulla exercitationem architecto sunt sint quis ipsam odio neque.',
+  },
+  {
+    id: 2,
+    icon: faCodeBranch,
+    name: 'Apps Development',
+    des: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione officiis iusto nulla exercitationem architecto sunt sint quis ipsam odio neque.',
+  },
+  {
+    id: 3,
+    icon: faCodeCompare,
+    name: 'Python Customize',
+    des: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione officiis iusto nulla exercitationem architecto sunt sint quis ipsam odio neque.',
+  },
+];
+
+import protfoliImg1 from '../../src/assets/p1.jpg';
+import protfoliImg2 from '../../src/assets/p2.jpg';
+import protfoliImg3 from '../../src/assets/p3.jpg';
+export const portfolio = [
+  {
+    id: 1,
+    img: protfoliImg1,
+    name: 'Project-1',
+    des: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione officiis iusto nulla exercitationem',
+    github: '#',
+  },
+  {
+    id: 1,
+    img: protfoliImg2,
+    name: 'Project-2',
+    des: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione officiis iusto nulla exercitationem',
+    github: '#',
+  },
+  {
+    id: 1,
+    img: protfoliImg3,
+    name: 'Project-3',
+    des: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione officiis iusto nulla exercitationem',
+    github: '#',
+  },
+];
+
+import avatar1 from '../../src/assets/avatar1.png';
+import avatar2 from '../../src/assets/avatar2.png';
+import avatar3 from '../../src/assets/avatar3.png';
+import avatar4 from '../../src/assets/avatar4.png';
+export const testimonials = [
+  {
+    id: 1,
+    img: avatar1,
+    name: 'Dong Lee',
+    des: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione officiis iusto nulla exercitationem architecto sunt sint quis ipsam odio neque.',
+  },
+  {
+    id: 2,
+    img: avatar2,
+    name: 'Mohammed Habib',
+    des: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione officiis iusto nulla exercitationem architecto sunt sint quis ipsam odio neque.',
+  },
+  {
+    id: 3,
+    img: avatar3,
+    name: 'Jhon Alas',
+    des: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione officiis iusto nulla exercitationem architecto sunt sint quis ipsam odio neque.',
+  },
+  {
+    id: 4,
+    img: avatar4,
+    name: 'Irfan Nazir',
+    des: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione officiis iusto nulla exercitationem architecto sunt sint quis ipsam odio neque.',
+  },
+];
---
+++ src/router/router.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const router = () => {
+  return <div>router</div>;
+};

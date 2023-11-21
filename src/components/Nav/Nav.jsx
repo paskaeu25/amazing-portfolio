1449a60fc279d018e3fbcfb82d43806b9a060515
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2023-11-21 17:21:03 +0200

    added nav items to navbar components

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,31 @@
 import './Nav.css';
 
+// Router Dom Import
 import { NavLink } from 'react-router-dom';
 
+// Fontawesome Import
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+// Data Import
+import { navLinks } from '../../data/Data';
+
 export default function Nav() {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <ul className="navlinks">
+        {navLinks.map(({ id, name, icon, path }) => {
+          return (
+            <li className="nav-item" key={id}>
+              <NavLink to={path}>
+                <span>
+                  <FontAwesomeIcon icon={icon} />
+                </span>
+                <h3>{name}</h3>
+              </NavLink>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
 }

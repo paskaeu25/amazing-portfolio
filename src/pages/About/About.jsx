df952111df4d03ffc6e74ce9d8cb726673887fc9
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2023-11-25 10:53:12 +0200

    added About page left section html

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,5 +1,49 @@
 import './About.css';
 
+// FontAwesome import
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+
+// CV import
+import CV from '../../assets/CV.pdf';
+
+// Component import
+import HeadingText from '../../components/HeadingText/HeadingText';
+
 export default function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <section className="About">
+        <HeadingText title={'About'} titlePrimary={'Me'} />
+
+        <div className="container about-container">
+          <div className="about-left">
+            <h3>Hi, I'm Pavelas Bulatovas</h3>
+            <p>
+              I decided to start my professional career in IT by learning
+              Business Analysis by myself. As a result of that, I had the
+              opportunity to work as an IT and Business Analyst for 3 years,
+              where I discovered my passion to materialize my and every
+              customer's ideas. Being always passionate about software
+              engineering, I decided to transition to web development to start a
+              new phase, taking advantage of all my business analysis experience
+              to provide effective solutions to complex problems. When I'm not
+              coding. you can find me working out, cycling, walking outside or
+              spending time with my friends.
+            </p>
+            <p>
+              When I'm not coding. you can find me working out, cycling, walking
+              outside or spending time with my friends.
+            </p>
+            <a href={CV} className="btn" download>
+              Download CV
+              <span>
+                <FontAwesomeIcon icon={faDownload} />
+              </span>
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }

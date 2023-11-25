ae11b626c79518aee7a662d8a96755c29605c5ce
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2023-11-25 10:23:28 +0200

    done theme toggle functionality

--- src/components/Theme/Theme.jsx
+++ src/components/Theme/Theme.jsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import './Theme.css';
+
+// Fontawesome import
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faN } from '@fortawesome/free-solid-svg-icons';
+
+// UseEffect & UseState import
+import { useState, useEffect } from 'react';
+
+// Use Local Storage For Theme
+const getLocalStorageTheme = () => {
+  let theme = 'light-theme';
+  if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme');
+  }
+
+  return theme;
+};
 
 export default function Theme() {
-  return <div>Theme</div>;
+  const [Theme, setTheme] = useState(getLocalStorageTheme());
+
+  const toggleTheme = () => {
+    Theme === 'light-theme' ? setTheme('dark-theme') : setTheme('light-theme');
+  };
+
+  useEffect(() => {
+    document.documentElement.className = Theme;
+    localStorage.setItem('theme', Theme);
+  }, [Theme]);
+
+  return (
+    <span className="Bg-toggler" onClick={toggleTheme}>
+      <FontAwesomeIcon icon={faMoon} />
+    </span>
+  );
 }

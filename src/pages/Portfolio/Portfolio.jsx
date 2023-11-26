0b8d1d187e2402c3e7f03084ee23b333917be95e
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2023-11-26 15:42:39 +0200

    created portfolio page projects section and styled it

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import './Portfolio.css';
+
+// Components import
+import Card from '../../UI/Card';
+import HeadingText from '../../components/HeadingText/HeadingText';
+
+// Data import
+import { portfolio } from '../../data/Data';
 
 export default function Portfolio() {
-  return <div>Portfolio</div>;
+  return (
+    <section className="portfolio">
+      <HeadingText title={'Resent'} titlePrimary={'Work'} />;
+      <div className="container portfolio-container">
+        {portfolio.map(({ id, img, name, des, github }) => {
+          return (
+            <Card classname="project" key={id}>
+              <img src={img} alt="" />
+              <h3>{name}</h3>
+              <p>{des}</p>
+              <a href={github}>Github</a>
+            </Card>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
